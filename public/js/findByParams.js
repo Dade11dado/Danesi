6fb833fb85b75115c59489a7937396f8aa1ef21d
Davide Vavassori <davidevavassori@MacBook-Pro-di-Davide.local>
2025-02-11 12:25:53 +0100

    trying console

--- public/js/findByParams.js
+++ public/js/findByParams.js
@@ -7,6 +7,7 @@ const fields = document.getElementById("divTable")
 var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour:"2-digit", minute:"2-digit" };
 
 buttonS.addEventListener("click", async ()=>{
+    console.log(process.env.FETCH_URL)
     spinner.style.display = "block"
     const response = await fetch(`${process.env.FETCH_URL}/transaction/findBy/?cardNumber=${number.value.trim()}&dateTo=${dateTo.value}&dateFrom=${dateFrom.value}`)
     const obj = await response.json()
