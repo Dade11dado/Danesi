f6cd90053b7c200ec522493ad2883fdbf9bbbbfd
Davide Vavassori <davidevavassori@MacBook-Pro-di-Davide.local>
2025-02-11 12:29:13 +0100

    corrected fetch

--- public/js/recharge.js
+++ public/js/recharge.js
@@ -106,7 +106,7 @@ importo.style.display="none"
 }
 
 async function postCard(){
-    await fetch(`${process.env.FETCH_URL}/card/insert`,{
+    await fetch(`$ https://mighty-oasis-93934-d4a0b00bbc93.herokuapp.com/card/insert`,{
         method:"POST",
         body: new URLSearchParams({cardId:inputNumber.value})
     })
@@ -115,7 +115,7 @@ async function postCard(){
 
 async function getInfo(){
     let response
-    response = await fetch(`${process.env.FETCH_URL}/card/find`,{
+    response = await fetch(` https://mighty-oasis-93934-d4a0b00bbc93.herokuapp.com/card/find`,{
         method:"POST",
         body:new URLSearchParams({cardId:inputNumber.value})
     })
@@ -134,7 +134,7 @@ async function chargeCard(){
         return
     }
     let response
-    response = await fetch(`${process.env.FETCH_URL}/card/recharge`,
+    response = await fetch(` https://mighty-oasis-93934-d4a0b00bbc93.herokuapp.com/card/recharge`,
         {method:"POST",
             body:new URLSearchParams({typeCard:type,cardId:cardId,total:total})
         }
